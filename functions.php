ff1b51e722283928b360c89f58c910c38d8e6e86
Harry Chappell <harry@hcdigital.co.uk>
2022-08-12 17:34:06 +0100

    Initial

---
+++ functions.php
@@ -0,0 +1,87 @@
+<?php
+
+require_once( 'library/bones.php' );
+
+
+function bones_ahoy() {
+
+  //Allow editor style.
+  add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' );
+
+  // let's get language support going, if you need it
+  load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
+
+  // launching operation cleanup
+  add_action( 'init', 'bones_head_cleanup' );
+  // A better title
+  add_filter( 'wp_title', 'rw_title', 10, 3 );
+  // remove WP version from RSS
+  add_filter( 'the_generator', 'bones_rss_version' );
+  // remove pesky injected css for recent comments widget
+  add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
+  // clean up comment styles in the head
+  add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
+  // clean up gallery output in wp
+  add_filter( 'gallery_style', 'bones_gallery_style' );
+
+  // enqueue base scripts and styles
+  add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+  // ie conditional wrapper
+
+  // launching this stuff after theme setup
+  bones_theme_support();
+
+  // adding sidebars to Wordpress (these are created in functions.php)
+  add_action( 'widgets_init', 'bones_register_sidebars' );
+
+  // cleaning up random code around images
+  add_filter( 'the_content', 'bones_filter_ptags_on_images' );
+  // cleaning up excerpt
+  add_filter( 'excerpt_more', 'bones_excerpt_more' );
+
+} /* end bones ahoy */
+
+add_action( 'after_setup_theme', 'bones_ahoy' );
+
+
+
+
+// Sidebars & Widgetizes Areas
+function bones_register_sidebars() {
+	register_sidebar(array(
+		'id' => 'sidebar1',
+		'name' => __( 'Sidebar 1', 'bonestheme' ),
+		'description' => __( 'The first (primary) sidebar.', 'bonestheme' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+}
+
+
+
+
+
+// Add browser class to body
+add_filter('body_class','browser_body_class');
+function browser_body_class($classes) {
+global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+if($is_lynx) $classes[] = 'lynx';
+elseif($is_gecko) $classes[] = 'gecko';
+elseif($is_opera) $classes[] = 'opera';
+elseif($is_NS4) $classes[] = 'ns4';
+elseif($is_safari) $classes[] = 'safari';
+elseif($is_chrome) $classes[] = 'chrome';
+elseif($is_IE) $classes[] = 'ie';
+else $classes[] = 'unknown';
+if($is_iphone) $classes[] = 'iphone';
+return $classes;
+}
+
+
+
+
+
+?>
