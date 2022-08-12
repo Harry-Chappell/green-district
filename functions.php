14da729dd3b38f8ee9ab9d80b2906c94044a384c
Harry Chappell <harry@hcdigital.co.uk>
2022-08-12 18:00:13 +0100

    Adding in custom templates and post type inc archive

--- functions.php
+++ functions.php
@@ -84,4 +84,67 @@ return $classes;
 
 
 
+// Custom Post Type  
+function custom_post_type() {
+  
+	// Set UI labels for Custom Post Type
+		$labels = array(
+			'name'                => _x( 'Stores', 'Post Type General Name'),
+			'singular_name'       => _x( 'Store', 'Post Type Singular Name'),
+			'menu_name'           => __( 'Stores'),
+			'parent_item_colon'   => __( 'Parent Store'),
+			'all_items'           => __( 'All Stores'),
+			'view_item'           => __( 'View Store'),
+			'add_new_item'        => __( 'Add New Store'),
+			'add_new'             => __( 'Add New'),
+			'edit_item'           => __( 'Edit Store'),
+			'update_item'         => __( 'Update Store'),
+			'search_items'        => __( 'Search Store'),
+			'not_found'           => __( 'Not Found'),
+			'not_found_in_trash'  => __( 'Not found in Trash'),
+		);
+		  
+	// Set other options for Custom Post Type
+		  
+		$args = array(
+			'label'               => __( 'stores'),
+			'description'         => __( 'Store listings'),
+			'labels'              => $labels,
+			// Features this CPT supports in Post Editor
+			'supports'            => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields', ),
+			/* A hierarchical CPT is like Pages and can have
+			* Parent and child items. A non-hierarchical CPT
+			* is like Posts.
+			*/
+			'hierarchical'        => false,
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'show_in_nav_menus'   => true,
+			'show_in_admin_bar'   => true,
+			'menu_position'       => 5,
+			'can_export'          => true,
+			'has_archive'         => true,
+			'exclude_from_search' => false,
+			'publicly_queryable'  => true,
+			'capability_type'     => 'post',
+			'show_in_rest' => true,
+	  
+		);
+		  
+		// Registering your Custom Post Type
+		register_post_type( 'stores', $args );
+	  
+	}
+	  
+	/* Hook into the 'init' action so that the function
+	* Containing our post type registration is not 
+	* unnecessarily executed. 
+	*/
+	  
+	add_action( 'init', 'custom_post_type', 0 );
+
+
+
+
 ?>
