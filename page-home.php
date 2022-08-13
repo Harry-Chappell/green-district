86098df3a3642f06d06173fb311b95388370ed9d
Harry Chappell <harry@hcdigital.co.uk>
2022-08-13 14:37:32 +0100

    Slider styling & adding dots & auto-advance slides

--- page-home.php
+++ page-home.php
@@ -21,7 +21,7 @@
 
 			<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-				<section class="hero">
+				<section class="hero" id="hero">
 					
 					<?php
 					$loop = new WP_Query(
@@ -36,7 +36,17 @@
 					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 					
 					<div class="hero-slide" style="background-image: url('<?php echo $image[0]; ?>')">
-						<?php the_title(); ?>
+						<div class="overlay <?php the_field('overlay_position'); ?> has-stripes--green">
+							<h2><?php the_title(); ?></h2>
+
+							<?php if( get_field('subtitle') ): ?>
+								<h3><?php the_field('subtitle'); ?></h3>
+							<?php endif; ?>
+
+							<?php if( get_field('link') ): ?>
+								<a href="<?php the_field('link'); ?>" class="button" title="<?php the_field('link_text'); ?>"><?php the_field('link_text'); ?></a>
+							<?php endif; ?>
+						</div>
 					</div>
 					
 					<?php endwhile;
@@ -45,8 +55,8 @@
 
 
 					<!-- Next and previous buttons -->
-					<a class="prev" onclick="plusSlides(-1)">left</a>
-					<a class="next" onclick="plusSlides(1)">right</a>
+					<a class="prev" onclick="plusSlides(-1)"></a>
+					<a class="next" onclick="plusSlides(1)"></a>
 
 					<!-- The dots/circles -->
 					<?php $slide_count = wp_count_posts( 'hero-slides' )->publish; ?>
@@ -61,23 +71,56 @@
 					?>
 
 					<script>
-						let slideIndex = 1;
-						showSlides(slideIndex);
+						
+						var slideIndex = 1;
 
-						// Next/previous controls
-						function plusSlides(n) {
-						showSlides(slideIndex += n);
+						var myTimer;
+
+						var slideshowContainer;
+
+						window.addEventListener("load",function() {
+							showSlides(slideIndex);
+							myTimer = setInterval(function(){plusSlides(1)}, 5000);
+						
+							//COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+							slideshowContainer = document.getElementsByClassName('hero')[0];
+						
+							//UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+							// slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+						
+							// slideshowContainer.addEventListener('mouseenter', pause)
+							// slideshowContainer.addEventListener('mouseleave', resume)
+						})
+
+						// NEXT AND PREVIOUS CONTROL
+						function plusSlides(n){
+						clearInterval(myTimer);
+						if (n < 0){
+							showSlides(slideIndex -= 1);
+						} else {
+						showSlides(slideIndex += 1); 
+						}
+						
+						//COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+						
+						if (n === -1){
+							myTimer = setInterval(function(){plusSlides(n + 2)}, 5000);
+						} else {
+							myTimer = setInterval(function(){plusSlides(n + 1)}, 5000);
+						}
 						}
 
-						// Thumbnail image controls
-						function currentSlide(n) {
+						//Controls the current slide and resets interval if needed
+						function currentSlide(n){
+						clearInterval(myTimer);
+						myTimer = setInterval(function(){plusSlides(n + 1)}, 5000);
 						showSlides(slideIndex = n);
 						}
 
-						function showSlides(n) {
-						let i;
-						let slides = document.getElementsByClassName("hero-slide");
-						let dots = document.getElementsByClassName("dot");
+						function showSlides(n){
+						var i;
+						var slides = document.getElementsByClassName("hero-slide");
+						var dots = document.getElementsByClassName("dot");
 						if (n > slides.length) {slideIndex = 1}
 						if (n < 1) {slideIndex = slides.length}
 						for (i = 0; i < slides.length; i++) {
@@ -88,14 +131,19 @@
 						}
 						slides[slideIndex-1].style.display = "block";
 						dots[slideIndex-1].className += " active";
-
 						}
+
+						pause = () => {
+						clearInterval(myTimer);
+						}
+
+						resume = () =>{
+						clearInterval(myTimer);
+						myTimer = setInterval(function(){plusSlides(slideIndex)}, 5000);
+						}
+
 					</script>
-
 					
-
-
-
 				</section>
 
 			</main>
