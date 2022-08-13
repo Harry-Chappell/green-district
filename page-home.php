7cf543641e914c06c1684a540a42f94c9d5d27a7
Harry Chappell <harry@hcdigital.co.uk>
2022-08-13 18:22:20 +0100

    Coming Up Styled

--- page-home.php
+++ page-home.php
@@ -197,6 +197,47 @@
 					</div>
 				</section>
 
+				<section class="coming-up wrap">
+
+					<h2>What's Coming Up</h2>
+					<h4>Lorem ipsum dolar sit amet.</h4>
+
+					<div class="posts-cntr">
+
+					<?php
+						$loop = new WP_Query(
+							array(
+								'post_type' => 'post',
+								'order'     => 'ASC'
+							)
+						);
+						while ( $loop->have_posts() ) : $loop->the_post();
+						?>
+
+						<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+						
+						<div class="post">
+							<a href="<?php the_permalink(); ?>" class="image-cntr" alt="<?php the_title(); ?>">
+								<img class="image" src="<?php echo $image[0]; ?>" alt="<?php the_title(); ?> Thumbnail">
+							</a>
+							<h3><?php the_title(); ?></h3>
+							<?php the_excerpt(); ?>
+							<a class="button inverted small" href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>">Read More</a>
+						</div>
+						
+						<?php endwhile;
+						wp_reset_postdata();
+					?>
+					</div>
+					
+					<a href="/whats-on" class="button">View All</a>
+
+					<script>
+						
+					</script>
+
+				</section>
+
 			</main>
 
 		</div>
