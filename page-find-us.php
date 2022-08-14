1de031ec1eb10b5aeaf7e0cb0cb18b3523307a85
Harry Chappell <harry@hcdigital.co.uk>
2022-08-14 23:07:23 +0100

    Added a video embed

--- page-find-us.php
+++ page-find-us.php
@@ -71,6 +71,48 @@
 								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48663.14141814384!2d-1.6028025158489236!3d53.804824873375075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48795eb565578bb1%3A0xc1c9ebda90c9d719!2s44%20Park%20View%20Ave%2C%20Burley%2C%20Leeds%20LS4%202LH!5e0!3m2!1sen!2suk!4v1660512258484!5m2!1sen!2suk" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 							</div>
 
+							<div class="basic-video-embed">
+								<h4>Basic Video Embed</h4>
+								<iframe width="560" height="315" src="https://www.youtube.com/embed/h-PfBxoMq_4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+								</div>
+								
+							<div class="advanced-video-embed">
+								<h4>Advanced Video Embed</h4>
+									<p>autoplays, muted, with limited controls.</p>
+
+												<div id="muteYouTubeVideoPlayer"></div>
+
+								<script async src="https://www.youtube.com/iframe_api"></script>
+								<script>
+								function onYouTubeIframeAPIReady() {
+									var player;
+									player = new YT.Player('muteYouTubeVideoPlayer', {
+									videoId: 'h-PfBxoMq_4', // YouTube Video ID
+									width: 560, // Player width (in px)
+									height: 316, // Player height (in px)
+									playerVars: {
+										autoplay: 1, // Auto-play the video on load
+										controls: 0, // Show pause/play buttons in player
+										showinfo: 0, // Hide the video title
+										modestbranding: 1, // Hide the Youtube Logo
+										loop: 1, // Run the video in a loop
+										fs: 0, // Hide the full screen button
+										cc_load_policy: 0, // Hide closed captions
+										iv_load_policy: 3, // Hide the Video Annotations
+										autohide: 0, // Hide video controls when playing
+									},
+									events: {
+										onReady: function (e) {
+										e.target.mute();
+										},
+									},
+									});
+								}
+
+								// Written by @labnol
+								</script>
+							</div>
+
 						</main>
 
 				</div>
